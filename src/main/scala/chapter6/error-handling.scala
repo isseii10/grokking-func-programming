741c06a2fa5668c3262e2e1e4ff767ee2926a6aa
isseii10 <t1l.g2.o11g@gmail.com>
2024-02-18 21:20:16 +0900

    done chapter6

--- src/main/scala/chapter6/error-handling.scala
+++ src/main/scala/chapter6/error-handling.scala
@@ -159,3 +159,87 @@ def _addOrResign(
     newParsedShow.map(newShow => shows.appended(newShow))
   )
 }
+
+// OptionのListをListのOptionに畳み込む
+def parseShowsV4(rawShows: List[String]): Option[List[TvShow]] = {
+  val initialResult: Option[List[TvShow]] = Some(List.empty)
+  rawShows.map(parseShowV3).foldLeft(initialResult)(addOrResign)
+}
+
+// エラーの詳細がわかるようにする。Either
+
+def extractNameV2(rawShow: String): Either[String, String] = {
+  val bracketOpenIdx = rawShow.indexOf('(')
+  for {
+    name <-
+      if (1 < bracketOpenIdx) Right(rawShow.substring(0, bracketOpenIdx).trim)
+      else Left(s"Cannot extract name from $rawShow")
+  } yield name
+}
+def extractYearStartV2(rawShow: String): Either[String, Int] = {
+  val bracketOpenIdx = rawShow.indexOf('(')
+  val dashIdx = rawShow.indexOf('-')
+  for {
+    yearStr <-
+      if (bracketOpenIdx != -1 && bracketOpenIdx + 1 < dashIdx)
+        Right(rawShow.substring(bracketOpenIdx + 1, dashIdx))
+      else Left(s"Cannot extract yearStart from $rawShow")
+    year <- yearStr.toIntOption.toRight(s"Cannot parse $yearStr")
+  } yield year
+  // 上のfor内包表記にする前
+  // val yearStrEither = if () Right(rawShow.substring()) else Left(s"Cannot extract...")
+  // yearStrEither.map(yearStr => yearStr.toIntOption.toRight(s"Cannot parse...")).flatten
+  //   toIntOptionでOption[Int](つまりSomeかNone)になる。
+  //   Someになった時はtoRightできるが、Noneの時はtoRightできないのでエラーメッセージ(Cannot parse...)がLeftに行く。
+  //   ここまででEither[String, Either[String, Int]]になっているので、flattenでEither[String, Int]にする
+}
+def extractYearEndV2(rawShow: String): Either[String, Int] = {
+  val dashIdx = rawShow.indexOf('-')
+  val bracketCloseIdx = rawShow.indexOf(')')
+  for {
+    yearStr <-
+      if (dashIdx != -1 && dashIdx + 1 < bracketCloseIdx)
+        Right(rawShow.substring(dashIdx + 1, bracketCloseIdx))
+      else Left(s"Cannot extract yearEnd from $rawShow")
+    year <- yearStr.toIntOption.toRight(s"Cannot parse $yearStr")
+  } yield year
+}
+
+def extractSingleYearV2(rawShow: String): Either[String, Int] = {
+  val dashIdx = rawShow.indexOf('-')
+  val bracketOpenIdx = rawShow.indexOf('(')
+  val bracketCloseIdx = rawShow.indexOf(')')
+  for {
+    yearStr <-
+      if (
+        dashIdx == -1 && bracketOpenIdx != -1 && bracketOpenIdx < bracketCloseIdx
+      )
+        Right(rawShow.substring(bracketOpenIdx + 1, bracketCloseIdx))
+      else Left(s"Cannot extract singleYear from $rawShow")
+    year <- yearStr.toIntOption.toRight(s"Cannot parse $yearStr")
+  } yield year
+}
+
+def parseShowV4(rawShow: String): Either[String, TvShow] = {
+  for {
+    name <- extractNameV2(rawShow)
+    start <- extractYearStartV2(rawShow).orElse(extractSingleYearV2(rawShow))
+    end <- extractYearEndV2(rawShow).orElse(extractSingleYearV2(rawShow))
+  } yield TvShow(name, start, end)
+}
+
+def parseShowsV5(rawShows: List[String]): Either[String, List[TvShow]] = {
+  val initialResult: Either[String, List[TvShow]] = Right(List.empty)
+  rawShows.map(parseShowV4).foldLeft(initialResult)(addOrResignV2)
+}
+
+// all or nothingのエラー処理方針
+def addOrResignV2(
+    parsedShows: Either[String, List[TvShow]],
+    newParsedShow: Either[String, TvShow]
+): Either[String, List[TvShow]] = {
+  for {
+    shows <- parsedShows // Option[List[TvShow]]からList[TvShow]を取り出す
+    newShow <- newParsedShow // Option[TvShow]からTvShowを取り出す
+  } yield shows.appended(newShow) // ジェネレータがOption[]なので最後もOptionになる
+}
